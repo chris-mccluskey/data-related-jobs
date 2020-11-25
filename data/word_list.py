4865ad6d2d2511fc4cb45b4bff7b7108ec0cebd0
Chris McCluskey <cmccluskey@gmail.com>
2020-11-25 16:49:53 +0400

    first commit

---
+++ data/word_list.py
@@ -0,0 +1,49 @@
+prog_lang = ['R', 'Python', 'Python/R', 'Java', 'C++', 'C', 'C/C++', 'C#', 'Julia', 'Perl', 'Scripting', 'Linux', 'Matlab', 'JavaScript', 'Scala',
+'Octave', 'XML', 'Ruby', 'GO', 'Coding', 'CI/CD', 'GIT', 'GitHub', 'APIs', 'API', 'Docker', 'Shell', 'Shiny', 'Fuse', 'Scrum', 'React', 'ReactJS',
+'NodeJS', 'Ansible', 'Collections', 'Modules', 'Exceptions', 'OOP', 'Regex', 'Serialization', 'Pickle', 'Bash', 'HTTP', 'Scraping']
+
+analysis = ['Excel', 'Tableau', 'NumPy', 'Pandas', 'Matplotlib', 'Seaborn', 'D3.js', 'SAS', 'SPSS', 'D3', 'Spotfire', 'Stata', 'Power BI', 'Plotly',
+'Looker', 'Airflow', 'Vertica', 'Google Analytics', 'Google', 'Jupyter', 'Notebooks', 'Powerbi', 'scipy', 'Grafana', 'ArcGIS', 'ggplot', 'ggplot2',
+'Columnar', 'Wavefront', 'storytelling', 'Dashboard', 'Dashboards', 'Pipeline', 'Pipelines', 'ETL', 'Mining', 'Spreadsheet', 'Clean', 'Written', 'Detail',
+'Visualization', 'Visualizations', 'Report', 'Reports', 'Data Driven', 'Interpret', 'Interpretability', 'Extraction', 'KPIs', 'Reading', 'Extracting', 'Validating', 'Wrangling',
+'Graphs', 'Cleaning', 'EDA', 'Manipulation', 'Geospatial', 'Exploratory']
+
+machine_learning = ['sklearn', 'Scikit-learn', 'NLP', 'Natural', 'NLTK', 'Keras', 'TensorFlow', 'PyTorch', 'Theano', 'SageMaker', 'H2O', 'Caffe', 'OpenCV',
+'Recommend', 'Recommendation', 'Recommendations', 'Algorithms', 'PCA', 'Classification', 'Classified', 'AI/ML', 'Neural', 'Clustering', 'Unsupervised',
+'Supervised', 'Accuracy', 'Prediction', 'MXNet', 'Biometrics', 'spaCy', 'Dimensionality', 'XGBoost', 'Forest', 'Rating', 'CAFFE', 'Standardization',
+'Normalization', 'Generative', 'GAN', 'GANs', 'Okta', 'Deep', 'Deep-Learning', 'Ensemble', 'MLflow', 'Gradient', 'Gradient-Descent', 'Feature', 'Selection', 'Tree', 'Trees', 'Support Vector',
+'Reinforcement', 'Kaggle', 'CNNs', 'Convolutional', 'YOLO', 'LSTMs', 'ANNs', 'RNNs']
+
+database = ['SQL', 'NoSQL', 'MongoDB', 'MySQL', 'Postgres', 'Postgresql', 'Cassandra', 'Redshift', 'DynamoDB', 'Redis', 'Database', 'Query', 'Querying',
+'Queries', 'SSRS', 'RDBMS', 'Non-Relational', 'Relational', 'Mariadb']
+
+cloud = ['Cloud', 'Azure', 'AWS', 'Glue', 'Google Cloud', 'Google Cloud Platform (GCP)', 'Cloud-Based', 'Kinesis', 'EC2']
+
+edu = ['Bachelor', 'Bachelors', 'Degree', 'Master', 'PhD', 'Ph.D.', 'MBA', 'Masters', 'M.s', 'MA', 'STEM', 'MS/PHD', 'CS', 'MD', 'Computer Science',
+'Statistics', 'Mathematics', 'Math', 'Physics', 'Machine Learning', 'Economics', 'Software Engineer', 'Information System', 'Quantitative Finance',
+'Programming', 'BS', 'B.S', 'BA/BS']
+
+big_data = ['Hadoop', 'MapReduce', 'Spark', 'Pig', 'Hive', 'Shark', 'Oozie', 'ZooKeeper', 'Flume', 'Mahout', 'Teradata', 'BigQuery', 'Presto', 'Apache']
+
+lang = ['French', 'German', 'Spanish', 'Chinese', 'Japanese']
+
+other = ['Decision Making', 'Decision Makers',  'Medallia', 'Speaking', 'Communication', 'Communicating', 'Communicate', 'Research', 'Problem Solving',
+'Deadlines', 'Attention', 'Teamwork', 'Team', 'Collaborate', 'Collaborative', 'Collaboration', 'Collaborating', 'Coordinate',  'Strategy',  'Verbal',  'Stakeholders',
+'Production', 'Actionable', 'Enterprise', 'Client', 'Insurance',  'Marketing',  'Cross Functional', 'Functional', 'Sales', 'Deployment', 'Deploying',
+'Deploy', 'Government', 'Presentation', 'Presentations', 'Forcasting', 'Documentation', 'End-to-End', 'Revenue',
+'Architect', 'Microsoft',  'Mentor', 'Mentoring', 'Maintenance', 'Finance', 'Cutting Edge', 'Inference', 'Identifying', 'Source', 'Text', 'Corporate',
+'Large Scale', 'Business Intelligence (BI)', 'Automate',  'Extract', 'Presenting', 'Image', 'Recognition', 'Mission Critical',
+'Prototype', 'Prototypes', 'Insight', 'Integrating', 'Certification', 'Hypothesis', 'Matching', 'IOT', 'ROI', 'Trade', 'SR.', 'Maintains',
+ 'Work-From-Home', 'Pricing', 'Behaviors', 'Robotics', 'UX', 'E-Commerce', 'Informatics', 'HIPAA', 'CRM', 'Back-End',
+'Profitability', 'Phishing', 'LOB (Line of Business)', 'CRO (Conversion Rate Optimization)', 'Retention',  'Telemetry', 'Optimization']
+
+healthcare = ['Bioinformatics', 'CDISC', 'SDTM', 'Biology', 'Medical', 'Dental', 'Pharmaceutical', 'Genetics', 'Genomics', 'Biotech', 'Biopharma', 'EMR',
+'Biostatistician']
+
+finance = ['CFDs']
+
+stats = ['Chi-Squared', 'Linear Regression', 'Linear', 'Algebra', 'Equations', 'Combinatorics', 'Regression', 'Distribution', 'Distributions', 'AB Testing', 'A/B Testing', 'ANOVA', 'Pre-Post',
+'Polynomials', 'Multivariate', 'Time-series', 'Timeseries', 'Date', 'Covariance', 'Correlate', 'Correlation', 'Descriptive', 'Diagnostic', 'Predictive', 'Prescriptive', 'Analytics',
+'Analyze', 'Analytical', 'Trend', 'Trends', 'Guassian', 'Bayesian', 'Probability', 'Statistician', 'Statisticians', 'Hypotheses', 'Calculus', 'Population',
+'Populations', 'Causality', 'ARIMA (Autoregressive integrated moving average)', 'Sampling', 'Dispersion', 'Sample', 'Factorization', 'Stochastic', 'Probabilistic',
+'Monte', 'Carlo', 'Confidence', 'Markov', 'Differential', 'Inferential', 'Rank', 'Variance', 'Vector', 'Vectors', 'Matrix', 'Integration']
